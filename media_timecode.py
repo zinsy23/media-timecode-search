55170cdec71808f51d7e59c71c69cbde6c13f97c
zinsy23 <39246267+zinsy23@users.noreply.github.com>
2025-02-18 22:56:03 -0600

    Hotfix for contiguous_search mechanism

--- media_timecode.py
+++ media_timecode.py
@@ -1,27 +1,11 @@
 def contiguous_search(text, array):
-  text_words = text.split()
-  start_index = -1
-  end_index = -1
-
-  for i, word in enumerate(array):
-    if word == text_words[0]:
-      start_index = i
-      match_count = 1
-      for j in range(i + 1, len(array)):
-        if match_count == len(text_words):
-          end_index = j - 1
-          break
-        if array[j] == text_words[match_count]:
-          match_count += 1
-        else:
-          start_index = -1
-          match_count = 0
-          break
-    if start_index != -1 and end_index != -1:
-      break
-
-  return start_index, end_index
-
-text = "turn off my TV it automatically"
-array = ["when I turn off my", "TV it automatically turns off that accessory as well"]
-start, end = contiguous_search(text, array)
+    target = text.strip()
+    n = len(array)
+    
+    for start in range(n):
+        for end in range(start, n):
+            segment = " ".join(s.strip() for s in array[start:end+1])
+            if target in segment:
+                return (start, end)
+    
+    return (-1, -1)
