9c8e520c9da1b7cd14c3a479baf7737537d5f723
zinsy23 <39246267+zinsy23@users.noreply.github.com>
2025-02-19 13:01:16 -0600

    Read timecodes and texts from source SRT

--- media_timecode.py
+++ media_timecode.py
@@ -1,3 +1,5 @@
+import re
+
 def contiguous_search(text, array):
     target = text.strip()
     n = len(array)
@@ -5,7 +7,37 @@ def contiguous_search(text, array):
     for start in range(n):
         for end in range(start, n):
             segment = " ".join(s.strip() for s in array[start:end+1])
+
             if target in segment:
                 return (start, end)
     
     return (-1, -1)
+
+# regex timecode: \d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}
+# regex index: ^[0-9]+$
+def load_srt(source):
+    sourceSrt = open(source, "r").read().split("\n")
+    sourceTimecodes = []
+    sourceTexts = []
+    lastTimeIndex = -1
+
+    for i in range(len(sourceSrt)):
+        if(re.search("\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}", sourceSrt[i])):
+            lastTimeIndex = i
+            sourceTimecodes.append(sourceSrt[i])
+        elif(sourceSrt[i] == ''):
+            sourceTexts.append(" ".join(sourceSrt[lastTimeIndex + 1:i]))
+
+        
+
+    
+    print(sourceTexts)
+
+load_srt("subtitles/time_travel.srt")
+
+'''
+text = "awesome there's"
+array = ["when I turn off my", "TV it automatically turns off that accessory as well", "that is awesome", "there's good stuff here"]
+
+start, end = contiguous_search(text, array)
+'''
