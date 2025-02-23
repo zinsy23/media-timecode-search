d13a0a593f19791aba69f2426bfccfef0db00099
zinsy23 <39246267+zinsy23@users.noreply.github.com>
2025-02-23 17:50:20 -0600

    Added commentary for features

--- media_timecode.py
+++ media_timecode.py
@@ -1,32 +1,35 @@
 import re
 import math
 
+# Searches blocks of subtitle text
 def contiguous_search(text, array):
-    target = text.strip()
+    target = text.strip() # Remove spaces at beginning and end of input text
     n = len(array)
     start = 0
-    
-    # print([row[1] for row in array[0:4]])
 
     for end in range(n):
+        # Combines multiple lines of subtitle input based on start and end range for comparison
         segment = " ".join(s.strip() for s in [row[1] for row in array[start:end+1]])
 
+        # If we have a match, adjust the start range to the true start of the match.
+        # Otherwise, keep going until there is a match
         if target in segment:
+            # Adjust the start to the very end and expand the search until we find our true start
             start = end - 1
             segment = " ".join(s.strip() for s in [row[1] for row in array[start:end+1]])
             while (target not in segment):
                 start = start - 1
-                print(start)
                 segment = " ".join(s.strip() for s in [row[1] for row in array[start:end+1]])
             return (start, end)
         else:
-            start = max(end - 10, math.ceil(len(text.split()) / 15))
+            # Memory management to prevent the segment variable from growing too much.
+            start = max(end - 10, math.ceil(len(text.split()) / 15)) # Start is a min of 10 but can grow if str(text) requires a greater gap to end
     
     return (-1, -1)
 
-# regex timecode: \d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}
-# regex index: ^[0-9]+$
+# Loads an SRT file into a data structure friendly for the rest of the program
 def load_srt(source):
+    # Read the input SRT and set up data structure friendly for all needed operations
     sourceSrt = open(source, "r").read().split("\n")
     sourceTimeTexts = []
     currentTimeText = []
@@ -34,10 +37,14 @@ def load_srt(source):
 
     for i in range(len(sourceSrt) - 1):
         if(re.search("\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}", sourceSrt[i])):
+            # Add timecode text entry to array
             lastTimeIndex = i
             currentTimeText.append(sourceSrt[i])
         elif(sourceSrt[i] == ''):
+            # Add correspondingtext entry to array
             currentTimeText.append(" ".join(sourceSrt[lastTimeIndex + 1:i]))
+
+            # When we have a time and text entry, we're read to put into 2D array
             if(len(currentTimeText) > 1):
                 sourceTimeTexts.append(currentTimeText)
                 currentTimeText = []
